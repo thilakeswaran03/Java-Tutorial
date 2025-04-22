42d0d08776c0177208c6058921e641b00133f309
thilakeswaran03 <thilakeswarankp@gmail.com>
2025-04-22 13:57:47 +0530

    base code

--- String/stringbuffer/Main.java
+++ String/stringbuffer/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Thilakes");
+        StringBuffer sb = new StringBuffer("Thilakes");
         System.out.println("original String: "+sb);
 
         //append
